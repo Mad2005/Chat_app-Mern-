de5299564d6fcb508286c3bf7720af4ce805dda9
Mad2005 <149214830+Mad2005@users.noreply.github.com>
2024-08-13 22:10:18 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -16,6 +16,7 @@ app.use(cors(
   {
     origin: ["https://chat-app-mern-frontend-jet.vercel.app"],
     methods: ["POST","GET"],
+    allowedHeaders: ["Content-Type", "Authorization"], 
     credentials: true
   }
 ));
