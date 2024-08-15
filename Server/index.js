be448e187edadd8eba3fc051c65274c87cf6dcb9
Mad2005 <149214830+Mad2005@users.noreply.github.com>
2024-08-15 23:08:36 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: "*",
+  origin: "https://chat-app-mern-frontend-jet.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
