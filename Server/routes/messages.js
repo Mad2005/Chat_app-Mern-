02cc3623c8539ffabe85c8580c5a1c35222128e3
Mad2005 <149214830+Mad2005@users.noreply.github.com>
2024-08-16 12:20:28 +0530

    Delete Server directory

--- Server/routes/messages.js
+++
@@ -1,7 +0,0 @@
-import { addMessage, getMessages } from "../controllers/messageController.js";
-import express from "express";
-const router = express.Router();
-router.post("/addmsg/", addMessage);
-router.post("/getmsg/", getMessages);
-
-export default router; 
